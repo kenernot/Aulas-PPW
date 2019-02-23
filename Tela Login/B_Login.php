db05f3a0e268820d2c44febed0bc65e2b7e4593d
kenernot <lukas_jacoby@hotmail.com>
2019-02-22 22:17:23 -0300

    Fixed SQL Injection

--- Tela Login/B_Login.php
+++ Tela Login/B_Login.php
@@ -1,16 +1,17 @@
 <?php
 include('includes/conecta.inc.php');
 session_start(); 
-$usuario = $_POST["usuario"]; 
+$usuario = md5($_POST["usuario"]); 
 $senha =  md5($_POST["senha"]); 
 
-$ver = "SELECT * from USUARIO where USUARIO = '$usuario' and SENHA = '$senha'";
+$ver = "SELECT idUsuario, usuario AS 'usuario', senha, nivel, dataCadastro, imagemPerfil, email from USUARIO where MD5(usuario) = '$usuario' and SENHA = '$senha';";
+echo $ver;
 $result_ver = mysql_query($ver,$conexao);
 $n_ver = mysql_num_rows($result_ver);
 
 if($n_ver!=0){
 		$dados = @mysql_fetch_array($result_ver); 
-		$_SESSION["usuario"]= $dados["usuario"]; 
+		$_SESSION["usuario"]= $dados["usuario"];
 		$_SESSION["nivel"] = $dados["nivel"]; 
 		$_SESSION["idUsuario"] = $dados["idUsuario"]; 
 		header("Location: index.php"); 
@@ -18,7 +19,7 @@ if($n_ver!=0){
 	/*?>
 	<script language="JavaScript">
 		alert("Usuário/Senha Incorretos, favor verificar!!!");
-		//window.history.go(-1);
+		window.history.go(-1);
 	</script>
 	<?php */
 	header("Location: F_Login.php");
